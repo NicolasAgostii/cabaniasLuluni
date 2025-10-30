8c113ebe78fe9e79100c418d0fcafb68b45e066f
Nicolas Agosti <agostinicolas6557@gmail.com>
2025-10-30 13:53:52 -0300

    Keep only frontend folder

--- cabanias-luluni/src/main/java/com/nicolas/cabanias/luluni/cabanias_luluni/repositories/CabaniaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nicolas.cabanias.luluni.cabanias_luluni.repositories;
-
-
-import com.nicolas.cabanias.luluni.cabanias_luluni.entities.Cabania;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CabaniaRepository extends JpaRepository<Cabania, Long> {
-}
