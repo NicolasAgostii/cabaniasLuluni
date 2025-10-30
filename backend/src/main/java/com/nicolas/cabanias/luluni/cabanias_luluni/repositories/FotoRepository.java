80db79ca20a813c471f8ee5d4c467ee13b17c03b
Nicolas Agosti <agostinicolas6557@gmail.com>
2025-10-30 13:55:25 -0300

    Solo front

--- backend/src/main/java/com/nicolas/cabanias/luluni/cabanias_luluni/repositories/FotoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nicolas.cabanias.luluni.cabanias_luluni.repositories;
-
-import com.nicolas.cabanias.luluni.cabanias_luluni.entities.Foto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FotoRepository extends JpaRepository<Foto, Long> {
-}
